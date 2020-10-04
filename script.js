0077691892ad5f301932d46e4ac3856bc73a733e
sarthak jain <sarthak@1gen.io>
2020-10-04 20:29:20 +0530

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var g = require('lodash');
+console.log(g);
+var arr = [1,2,3,45,7];
+console.log(g.without(arr , 3));
+var css = document.querySelector("h3");
+
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
